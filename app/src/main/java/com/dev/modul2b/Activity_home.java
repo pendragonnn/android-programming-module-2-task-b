1eb99227cc67f5e7084e9a3685d2d144e869cf30
Wisnu Andika <wisnuandika104@gmail.com>
2024-02-15 22:15:53 +0700

    Initial commit

---
+++ app/src/main/java/com/dev/modul2b/Activity_home.java
@@ -0,0 +1,14 @@
+package com.dev.modul2b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
